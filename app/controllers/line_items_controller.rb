62adfd75901bc812a7e4e787b606501cb9d90b0a
Roberto <Roberto@iMac.local>
2016-12-17 22:11:36 +0100

    Cart Handling Errors

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
     # @line_item = LineItem.new(line_item_params)
     ##@line_item = @cart.line_items.build(product: product)
     @line_item = @cart.add_product(product)
-    
+
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
@@ -74,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
